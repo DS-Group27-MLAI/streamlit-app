88ae8aeb23a07dc52dbb665b822e2d442df080d0
Aswin Vijayakumar <aswinkvj@gmail.com>
2022-02-28 18:03:56 +0000

    req and setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,7 @@ port = $PORT\n\
 enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
+
+mkdir -p models/
+
+wget https://ds-group22-adp.s3.eu-west-2.amazonaws.com/mlai/models/model_best_weights_anomaly_vae.h5 -o models/model_best_weights_anomaly_vae.h5

70e1f64d39d8f001b04de50a9b9f23e5aef3874e
AnoopTanwar <anupsingh.com@gmail.com>
2022-03-08 15:32:29 +0000

    Update app.py
    Added Updated Code

--- app.py
+++ app.py
@@ -1,39 +1,16 @@
 import streamlit as st
-from classification import classification_inference
-from anomaly_detection import anomaly_detection_inference
-PAGE_CONFIG = {"page_title":"StColab.io","page_icon":":smiley:","layout":"centered"}
-st.set_page_config(**PAGE_CONFIG)
-
-def main():
-  st.title("Image Anomaly Detection")
-  menu = ['Video', "About", "Classification Inference", "Anomaly Detection Inference"]
-  choice = st.sidebar.selectbox('Menu', menu)
-
-add_home = st.sidebar.header("[Home](https://wandererluzon.wixsite.com/my-site)")
-add_readme = st.sidebar.header("[Read Me](https://wandererluzon.wixsite.com/my-site)")
-add_about = st.sidebar.header("[About](https://wandererluzon.wixsite.com/my-site)")
-add_members = st.sidebar.header("[Members](https://wandererluzon.wixsite.com/my-site)")
-
-
-
-option_selected = st.radio(
-    "Select the Image processing method",
-    ('Anomaly Detection', 'Classification of Image'))
-if option_selected == 'Anomaly Detection':
-    st.write('Anomaly Detection is selected')
-    anomaly_detection_inference()
-else:
-    st.write("Classification of Image is selected")
-    classification_inference()
-
-#if choice == "Video":
-#    st.video(open("PNG_9048.mp4", 'rb'), format='video/mp4', start_time=0)
-
-#if choice == "Classification Inference":
-#    classification_inference()
-#if choice == "Anomaly Detection Inference":
- #   anomaly_detection_inference()
-    
-if __name__ == "__main__":
-  
- main()
+import home
+import readme
+import members
+import about
+from multiapp import MultiApp
+app = MultiApp()
+st.sidebar.title('Navigation')
+st.sidebar.markdown('Select to Navigate')
+# Add all your application here
+app.add_app("Home", home.app)
+app.add_app("Read Me", readme.app)
+app.add_app("About", about.app)
+app.add_app("Members", members.app)
+# The main app
+app.run()
